cb155ffcb76e96ab99c2ec63aa92fb734281d609
linobrown99 <163183584+linobrown99@users.noreply.github.com>
2024-04-11 13:27:15 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+document.querySelector("button").addEventListener("click",function () {
+    alert("i got clicked!");
+});
+let myHeading = document.querySelector("h2");
+myHeading.textContent = "Bonjour, monde de la beaute !";
+
